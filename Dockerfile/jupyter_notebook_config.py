ada58a5d2f06a833fe9ad158b69a444de8b0c38c
Bruno <brunoricardojava@gmail.com>
2017-09-01 17:16:40 -0300

    Add option for config jupyter notebook without enter the container

--- Dockerfile/jupyter_notebook_config.py
+++ Dockerfile/jupyter_notebook_config.py
@@ -3,3 +3,4 @@
 # Password to use for web authentication
 c = get_config()
 c.NotebookApp.password = u'sha1:aeed80c475e3:eec4bb455f68263fc6825a1b4626312790fc9af0'
+c.NotebookApp.iopub_data_rate_limit = 10000000
